610b5878ce481ad89d8306c9237863b668506ee8
Shefin S <shefin@cocoalabs.in>
2020-10-20 10:48:25 +0530

    changes

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps : [
     {
-      name: 'admin Gemeric ecom',
+      name: 'admin - Bakery-Ecommerce',
       script: 'admin.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -11,11 +11,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4002
+        port : 4302
       }
     },
     {
-      name: 'users - Generic ecom',
+      name: 'users - Bakery-Ecommerce',
       script: 'user.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -25,11 +25,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4008
+        port : 4308
       }
     },
     {
-      name: 'products - Generic ecom',
+      name: 'products - Bakery-Ecommerce',
       script: 'product.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -39,11 +39,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4006
+        port : 4306
       }
     },
     {
-      name: 'category - Generic ecom',
+      name: 'category - Bakery-Ecommerce',
       script: 'categories.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -53,11 +53,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4003
+        port : 4303
       }
     },
     {
-      name: 'address - Generic ecom',
+      name: 'address - Bakery-Ecommerce',
       script: 'address.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -67,11 +67,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4001
+        port : 4301
       }
     },
     {
-      name: 'order - Generic ecom',
+      name: 'order - Bakery-Ecommerce',
       script: 'order.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -81,11 +81,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4005
+        port : 4305
       }
     },
     {
-      name: 'review - Generic ecom',
+      name: 'review - Bakery-Ecommerce',
       script: 'review.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -95,11 +95,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4010
+        port : 4310
       }
     },
     {
-      name: 'feed back - Generic ecom',
+      name: 'feed back - Bakery-Ecommerce',
       script: 'feedback.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -109,11 +109,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4011
+        port : 4311
       }
     },
     {
-      name: 'carts - Generic ecom',
+      name: 'carts - Bakery-Ecommerce',
       script: 'cart.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -123,11 +123,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4012
+        port : 4312
       }
     },
     {
-      name: 'admin category - Generic ecom',
+      name: 'admin category - Bakery-Ecommerce',
       script: 'adminCategory.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -137,11 +137,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4021
+        port : 4313
       }
     },
     {
-      name: 'admin products - Generic ecom',
+      name: 'admin products - Bakery-Ecommerce',
       script: 'adminProducts.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -151,12 +151,12 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4022
+        port : 4314
       }
     }
     ,
     {
-      name: 'admin delivery - Generic ecom',
+      name: 'admin delivery - Bakery-Ecommerce',
       script: 'adminDeliveryPartner.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -166,11 +166,11 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4023
+        port : 4315
       }
     },
     {
-      name: 'app delivery - Generic ecom',
+      name: 'app delivery - Bakery-Ecommerce',
       script: 'appDeliveryPartner.service.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       instances: 1,
@@ -180,7 +180,7 @@ module.exports = {
       //cron_restart
       env: {
         NODE_ENV: 'development',
-        port : 4024
+        port : 4316
       }
     }
   
