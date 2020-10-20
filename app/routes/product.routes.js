26753a237f9d789526e39ebcdef0dc0ffe23142d
Shefin S <shefin@cocoalabs.in>
2020-10-20 10:55:23 +0530

    changes

--- app/routes/product.routes.js
+++ app/routes/product.routes.js
@@ -2,6 +2,6 @@ const auth = require('../middleware/auth.js');
 
 module.exports = (app) => { 
     const product = require('../controllers/product.controller');
-    app.get('/products', product.list);
-    app.get('/products/:id', product.detail);
+    app.get('/product', product.list);
+    app.get('/product/:id', product.detail);
 }
