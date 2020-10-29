b3d669c950a2bf4025b0fb5bd246fac27ae19a3f
Shefin S <shefin@cocoalabs.in>
2020-10-29 12:36:27 +0530

    changes

--- app/helpers/constants.js
+++ app/helpers/constants.js
@@ -9,5 +9,58 @@ module.exports = Object.freeze({
   veg: 'veg',
   nonVeg: 'nonVeg',
   combo: 'combo',
-  fastFood: 'fastFood'
+  fastFood: 'fastFood',
+
+
+  APP_LANG_MALAYALAM: "mal",
+  APP_LANG_ENGLISH: "eng",
+
+  TYPE_USER: "user",
+  TYPE_ADMIN: "admin",
+
+  ANSWER_TYPE_TEXT: "text",
+  ANSWER_TYPE_OPTION: "option",
+  ANSWER_TYPE_ADDRESS: "address",
+  ANSWER_TYPE_DIGIT: "digit",
+  OPTIION_YES: "yes",
+  APP_TYPE: "app",
+  IS_SELECTED_TRUE: true,
+  IS_SELECTED_FALSE: false,
+  ABOUT_US: 'about_us',
+  VERSION: 'version',
+
+  CREATE_ORDER_NOTIFICATION_TITLE : 'Order created',
+  CREATE_ORDER_NOTIFICATION_MESSAGE : 'Order created',
+
+  ASSIGN_ORDER_NOTIFICATION_TITLE : 'Order assigned',
+  ASSIGN_ORDER_NOTIFICATION_MESSAGE : 'Order assigned',
+
+  ACCEPTED_ORDER_NOTIFICATION_TITLE : 'Order accepted',
+  ACCEPTED_ORDER_NOTIFICATION_MESSAGE : 'Order accepted',
+
+  PICKED_ORDER_NOTIFICATION_TITLE : 'Order picked',
+  PICKED_ORDER_NOTIFICATION_MESSAGE : 'Order picked',
+
+  DELIVERED_ORDER_NOTIFICATION_TITLE : 'Order delivered',
+  DELIVERED_ORDER_NOTIFICATION_MESSAGE : 'Order delivered',
+
+  ORDER_NOTIFICATION : 'order',
+  ASSIGN_NOTIFICATION : 'assign',
+
+  ACTIVE_CART : 'active',
+
+  PENDING_ORDER : 'pending',
+  DISPATCHED_ORDER : 'dispatched',
+  CANCELLED_ORDER : 'cancelled',
+  ASSIGNED_ORDER : 'assigned',
+
+  ACCEPTED_ORDER : 'accepted',
+  PICKED_ORDER : 'picked',
+  DELIVERED_ORDER : 'delivered',
+  // DISPATCHED_ORDER : 'dispatched',
+  // ACTIVE_ORDER : 'active',
+
+  DECREMENT_STOCK : 'decrement',
+  INCREMENT_STOCK : 'increment',
+
 });
